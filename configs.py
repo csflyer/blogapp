0c3ccc6363758c2e4e4237901ac3bd080dc684a8
crazyliuh <924105150@qq.com>
2019-02-26 16:54:04 +0800

    add new-style form inheritation and aliyun mail send service
    Signed-off-by: liuhui <924105150@qq.com>

--- configs.py
+++ configs.py
@@ -8,7 +8,17 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    VALIDATE_CODE_FONT_PATH = '../static/STFANGSO.TTF'
     DEBUG = True
+
+    MAIL_SERVER = 'smtpdm.aliyun.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'admin@csflyer.com'
+    MAIL_PASSWORD = 'LiuHui5901214'
+    MAIL_SUBJECT_PREFIX = 'Crazyliu Blog'
+    MAIL_SENDER = 'admin@csflyer.com'
+
 
 class TestingConfig(Config):
     pass
@@ -24,3 +34,5 @@ config = {
     'testing': TestingConfig,
     'production': ProductionConfig
 }
+
+
--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 import os
 from app.models import User, Role, Post, Follow, ClassifyTag, Comment, ArticleTag
 from app import create_app, db
+from app.tools.send_mail import send_mail
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -11,7 +12,7 @@ migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, Post=Post, Follow=Follow, ClassifyTag=ClassifyTag,
-                Comment=Comment, ArticleTag=ArticleTag)
+                Comment=Comment, ArticleTag=ArticleTag, send_mail=send_mail)
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
